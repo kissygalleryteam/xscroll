460cd26a80467dbc405033bd69cba1be78eff29a
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-14 12:29:46 +0800

    sync daily version 1.1.10

---
+++ 1.1.10/build/plugin/mods-min.js
@@ -0,0 +1,2 @@
+/*!build time : 2014-10-14 11:38:22 AM*/
+KISSY.config("modules",{"kg/xscroll/1.1.10/plugin/scrollbar":{requires:["node","base","anim","kg/xscroll/1.1.10/util"]}});
---
+++ 1.1.10/build/plugin/mods.js
@@ -0,0 +1,3 @@
+KISSY.config('modules', {
+ 'kg/xscroll/1.1.10/plugin/scrollbar': { requires: ['node', 'base', 'anim', 'kg/xscroll/1.1.10/util']} 
+});
