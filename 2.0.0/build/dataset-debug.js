d148b8577949e8573efd60bb43525e975ca2c229
弘树 <tiehang.lth@alibaba-inc.com>
2014-10-20 14:53:04 +0800

    sync daily version 2.0.0

---
+++ 2.0.0/build/dataset-debug.js
@@ -0,0 +1,51 @@
+KISSY.add('kg/xscroll/2.0.0/dataset',[],function(S ,require, exports, module) {
+define(function(require, exports, module) {
+
+	var DataSet = function(cfg){
+
+		this.data = cfg && cfg.data || [];
+
+		this.id = cfg && cfg.id || "_ds_"+Date.now();
+
+	}
+
+	DataSet.prototype.appendData = function(data){
+		this.data = this.data.concat(data)
+	};
+
+	DataSet.prototype.insertData = function(index,data){
+		if(typeof index == "number"){
+			this.data.splice(index,0,data);
+		}
+	};
+
+	DataSet.prototype.removeData = function(index){
+		if(typeof index == "number" && this.data[index]){
+			this.data.splice(index,1);
+		}else{
+			this.data = [];
+		}
+	};
+
+	DataSet.prototype.getData = function(index){
+		if(typeof index == "number"){
+			return this.data[index];
+		}
+		return this.data;
+	};
+
+	DataSet.prototype.setId = function(id){
+		if(!id) return;
+		this.id = id;
+		return this.id;
+	};
+
+	DataSet.prototype.getId = function(){
+		return this.id;
+	};
+
+
+	return DataSet;
+
+})
+});
